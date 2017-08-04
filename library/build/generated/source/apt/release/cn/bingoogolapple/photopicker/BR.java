3de823b3024098a8f1fa80321cc71ce58cec47e8
lenovo <847436762@qq.com>
2017-08-04 16:48:33 +0800

    移除无效库

--- library/build/generated/source/apt/release/cn/bingoogolapple/photopicker/BR.java
+++
@@ -1,5 +0,0 @@
-package cn.bingoogolapple.photopicker;
-
-public class BR {
-        public static int _all = 0;
-}
--- library/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="7e380dd0-56ce-4da6-a469-0e7b1bd7a7ad")
-public class DataBindingInfo {}
